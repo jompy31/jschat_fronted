73643dda3c3561d899696bcf3af976af457d137a
Naomy Sandí Quirós <168208386+Naomy25sandi@users.noreply.github.com>
2025-10-16 14:58:16 -0600

    pagina error unificada a estilos y redirecciona a home

--- src/containers/errors/Error404.jsx
+++ src/containers/errors/Error404.jsx
@@ -1,26 +1,28 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Error404.css'; // Importamos el archivo CSS para las animaciones
+import './Error404.css';
+import Logo from '../../assets/LOGO_rectangular.png'; // Ajusta la ruta según tu proyecto
 
 function Error404() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        // Redirigir automáticamente después de 5 segundos
         const timer = setTimeout(() => {
             navigate('/');
         }, 5000);
-
-        // Limpiar el temporizador al desmontar el componente
         return () => clearTimeout(timer);
     }, [navigate]);
 
     return (
         <div className="error-container">
             <div className="error-message">
+                <img src={Logo} alt="Jsport Logo" className="brand-logo" />
                 <h1 className="error-code">404</h1>
-                <p className="error-description">Oops! La página de ABCupon que buscas no se encuentra disponible.</p>
-                <p className="redirect-message">Serás redirigido en 5 segundos...</p>
+                <p className="error-description">
+                    ¡Ups! La página que buscas en <strong>Jsport</strong> no está disponible.
+                </p>
+                <p className="redirect-message">Serás redirigido al inicio en 5 segundos...</p>
+                <a href="/" className="go-home-button">Ir al Inicio</a>
             </div>
         </div>
     );
