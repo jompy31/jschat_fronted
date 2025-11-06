587ba4eb995af50004bff756de09524e5b553de5
jompy31 <jpbcserviciotecnico@gmail.com>
2025-11-06 11:37:31 -0600

    cambios nginx

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode basename="/programas/jsportapp">
     <Provider store={store}>
       <BrowserRouter> 
         <App />
