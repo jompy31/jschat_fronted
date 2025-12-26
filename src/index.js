e9deb80f5c5e7cf452c1446e40192bc6300ae6b9
jompy31 <jpbcserviciotecnico@gmail.com>
2025-12-26 07:57:15 -0600

    tiendajsport

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
     <Provider store={store}>
-      <BrowserRouter basename="/programas/jsportapp"> 
+      <BrowserRouter basename="/"> 
         <App />
       </BrowserRouter>
     </Provider>
